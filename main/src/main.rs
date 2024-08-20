d0364dec13152273cfdc7638c5a5c98ecae4ca65
Gabriel <gabrielcampeanu@gmail.com>
2024-08-20 16:57:23 +0200

    ownership

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    let a_main: Box<i32> = Box::new(5);
+    let b = a_main;
+    //println!("{}", a_main);
+    move_a_box(b);
+}
+
+fn move_a_box(a: Box<i32>) {
+    println!("inside box fn");
 }
