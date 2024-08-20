0bf0d55d489902c8b8a09fd99a07e8397ff3c28a
Gabriel <gabrielcampeanu@gmail.com>
2024-08-20 11:51:44 +0200

    for loop

--- main/src/main.rs
+++ main/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
 println!("{}", test_rtrn1(4));
 println!("{}", test_rtrn2(4));
 println!("{}", test_break());
+
+let v = [1,2, 3, 4, 5];
+
+for elem in v {
+    println!("{}", elem);
+}
 }
 
 
