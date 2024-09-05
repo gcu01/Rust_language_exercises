6bb13c26ca57d9c316318dc9e7bec5625af7bca1
Gabriel <gabrielcampeanu@gmail.com>
2024-09-05 14:36:08 +0200

    generic structs - compiles with errors

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,23 @@
 use std::io::{self, Read};
 use std::fs::File;
+#[derive(Debug)]
+struct Point<T, TT> {
+    x: T,
+    y: TT,
+}
 
+impl<T, TT> Point<T, TT> {
+    fn get_x(&self) -> &T {
+        &self.x
+    }
+
+    fn make_other<L, LL> (&self, other: &Point<L, LL> ) -> Point <&T, &LL> {
+        Point {
+            x: &self.x,
+            y: &other.y,
+        }
+    }
+}
 fn main() {
 
    println!("{:?}", work_file());
@@ -9,6 +26,12 @@ fn main() {
         None => {println!("None");}
     }
 
+    let x1: Point<String, i32> = Point{x:"mama".to_string(), y: 10};
+    let x2: Point<i32, String> = Point {x: 12, y: "tata".to_string()};
+
+    let mut x3: Point<&str, &str> = Point::make_other(&x1, &x2);
+    println!("{x3:?}");
+    println!("{x1:?}");
 }
 
 fn work_file() -> Result<String, io::Error> {
